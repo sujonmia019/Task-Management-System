14c6ff5d44ff85a8acdfa195de705b03e677f6f5
sujon <sujonmyolbd@gmail.com>
2024-12-24 13:58:43 +0600

    Task API read, store, update, delete

--- routes/api.php
+++ routes/api.php
@@ -5,11 +5,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\TaskController;
 
+// Auth Routes
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 Route::middleware('auth:sanctum')->group(function () {
+    // Task Routes
     Route::prefix('tasks')->group(function(){
         Route::get('/', [TaskController::class,'index']);
         Route::post('store-or-update/{id?}', [TaskController::class,'storeOrUpdate']);
